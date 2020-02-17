5f026c37355a1492df1d79ad8c30a89f226e6f9b
Peter Smith <peter@upstateinteractive.io>
2020-02-16 19:16:50 -0500

    chore: configure scullyconfig

--- scully.scully-example-contentful.config.js
+++ scully.scully-example-contentful.config.js
@@ -1,7 +1,24 @@
+require('scully-contentful');
+require('dotenv').config();
+
 exports.config = {
   projectRoot: "./src",
   projectName: "scully-example-contentful",
   outDir: './dist/static',
   routes: {
+    '/article/:id': {
+      // Type is mandatory
+      type: 'contentful',
+      /**
+       * Every parameter in the route must exist here
+       */
+      config: {
+        space: process.env.SPACE_ID,
+        accessToken: process.env.ACCESS_TOKEN,
+        host: 'https://cdn.contentful.com',
+        environment: 'master',
+        proxy: null
+      }
+    },
   }
 };
